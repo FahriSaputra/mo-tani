477c01da3e6dac7caa9b9dbc2f4e52e7009f4028
Fahri Saputra <fahrisaputra190@gmail.com>
2021-12-24 14:52:22 +0700

    fix table on mobile devices

--- src/pages/CheckoutStatus.js
+++ src/pages/CheckoutStatus.js
@@ -31,53 +31,56 @@ const CheckoutStatus = () => {
           </div>
         </div>
       </div>
-
-      <table class="border-collapse border border-gray-400 w-full mt-5 mb-10">
-        <thead>
-          <tr>
-            <th class="border border-yellow-500 bg-yellow-500">id</th>
-            <th class="border border-yellow-500 bg-yellow-500">
-              Product Image
-            </th>
-            <th class="border border-yellow-500 bg-yellow-500">Product Name</th>
-            <th class="border border-yellow-500 bg-yellow-500">Total</th>
-            <th class="border border-yellow-500 bg-yellow-500">Quantity</th>
-            <th class="border border-yellow-500 bg-yellow-500">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {checkout?.map((item, index) => (
+      <div className="overflow-scroll lg:w-full">
+        <table class="border-collapse border border-gray-400 w-full mt-5 mb-10">
+          <thead>
             <tr>
-              <td class="border border-gray-300 text-center">{index + 1}</td>
-              <td class="border border-gray-300">
-                <div className="flex justify-center">
-                  <img
-                    src={`http://127.0.0.1:8000/storage/images/${item?.image}`}
-                    alt={item?.name}
-                    className="w-24"
-                  />
-                </div>
-              </td>
-              <td class="border border-gray-300 text-center">{item?.name}</td>
-              <td class="border border-gray-300 text-cente">{item?.total}</td>
-              <td class="border border-gray-300 text-center">
-                {item?.quantity}
-              </td>
-              <td
-                class={`border border-gray-300 text-center ${
-                  item?.status === "waiting"
-                    ? "text-yellow-300"
-                    : item?.status.toLowerCase() === "success"
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {item?.status}
-              </td>
+              <th class="border border-yellow-500 bg-yellow-500">id</th>
+              <th class="border border-yellow-500 bg-yellow-500">
+                Product Image
+              </th>
+              <th class="border border-yellow-500 bg-yellow-500">
+                Product Name
+              </th>
+              <th class="border border-yellow-500 bg-yellow-500">Total</th>
+              <th class="border border-yellow-500 bg-yellow-500">Quantity</th>
+              <th class="border border-yellow-500 bg-yellow-500">Status</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {checkout?.map((item, index) => (
+              <tr>
+                <td class="border border-gray-300 text-center">{index + 1}</td>
+                <td class="border border-gray-300">
+                  <div className="flex justify-center">
+                    <img
+                      src={`http://127.0.0.1:8000/storage/images/${item?.image}`}
+                      alt={item?.name}
+                      className="w-24"
+                    />
+                  </div>
+                </td>
+                <td class="border border-gray-300 text-center">{item?.name}</td>
+                <td class="border border-gray-300 text-cente">{item?.total}</td>
+                <td class="border border-gray-300 text-center">
+                  {item?.quantity}
+                </td>
+                <td
+                  class={`border border-gray-300 text-center ${
+                    item?.status === "waiting"
+                      ? "text-yellow-300"
+                      : item?.status.toLowerCase() === "success"
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }`}
+                >
+                  {item?.status}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </MainLayout>
   );
 };
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -58,79 +58,82 @@ const Dashboard = () => {
           <PrimaryButton onClick={onAddProduct} title="Add Product" />
         </div>
       </div>
-
-      <table class="border-collapse border border-gray-400 w-full mt-5 mb-10">
-        <thead>
-          <tr>
-            <th class="border border-yellow-500 bg-yellow-500">id</th>
-            <th class="border border-yellow-500 bg-yellow-500">Name</th>
-            <th class="border border-yellow-500 bg-yellow-500">Proof</th>
-            <th class="border border-yellow-500 bg-yellow-500 w-64">
-              Detail User
-            </th>
-            <th class="border border-yellow-500 bg-yellow-500">Product Name</th>
-            <th class="border border-yellow-500 bg-yellow-500">Total</th>
-            <th class="border border-yellow-500 bg-yellow-500">Quantity</th>
-            <th class="border border-yellow-500 bg-yellow-500">Status</th>
-            <th class="border border-yellow-500 bg-yellow-500">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {checkout?.map((item, index) => (
+      <div className="overflow-scroll lg:w-full">
+        <table class="border-collapse border border-gray-400 mt-5 mb-10 w-full">
+          <thead>
             <tr>
-              <td class="border border-gray-300 text-center">{index + 1}</td>
-              <td class="border border-gray-300 text-center">
-                {item?.fullname}
-              </td>
-              <td class="border border-gray-300">
-                <div className="flex justify-center">
-                  <img
-                    src={`http://127.0.0.1:8000/storage/images/${item?.proof}`}
-                    alt={item?.name}
-                    className="w-24"
-                  />
-                </div>
-              </td>
-              <td class="border border-gray-300 text-center">
-                {item?.handphone} / {item?.address}
-              </td>
-              <td class="border border-gray-300 text-center">{item?.name}</td>
-              <td class="border border-gray-300 text-cente">{item?.total}</td>
-              <td class="border border-gray-300 text-center">
-                {item?.quantity}
-              </td>
-              <td
-                class={`border border-gray-300 text-center ${
-                  item?.status === "waiting"
-                    ? "text-yellow-300"
-                    : item?.status.toLowerCase() === "success"
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {item?.status}
-              </td>
-              <td class="border border-gray-300 text-center">
-                {item?.status === "waiting" ? (
-                  <div className="p-3">
-                    <PrimaryButton
-                      title="Cancel"
-                      onClick={() => onCancelCheckout(item?.id)}
-                    />
-                    <div className="h-4" />
-                    <PrimaryButton
-                      title="Accept"
-                      onClick={() => onConfirmCheckout(item?.id)}
+              <th class="border border-yellow-500 bg-yellow-500">id</th>
+              <th class="border border-yellow-500 bg-yellow-500">Name</th>
+              <th class="border border-yellow-500 bg-yellow-500">Proof</th>
+              <th class="border border-yellow-500 bg-yellow-500 w-64">
+                Detail User
+              </th>
+              <th class="border border-yellow-500 bg-yellow-500">
+                Product Name
+              </th>
+              <th class="border border-yellow-500 bg-yellow-500">Total</th>
+              <th class="border border-yellow-500 bg-yellow-500">Quantity</th>
+              <th class="border border-yellow-500 bg-yellow-500">Status</th>
+              <th class="border border-yellow-500 bg-yellow-500">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {checkout?.map((item, index) => (
+              <tr>
+                <td class="border border-gray-300 text-center">{index + 1}</td>
+                <td class="border border-gray-300 text-center">
+                  {item?.fullname}
+                </td>
+                <td class="border border-gray-300">
+                  <div className="flex justify-center">
+                    <img
+                      src={`http://127.0.0.1:8000/storage/images/${item?.proof}`}
+                      alt={item?.name}
+                      className="w-24"
                     />
                   </div>
-                ) : (
-                  <p>Done</p>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                </td>
+                <td class="border border-gray-300 text-center">
+                  {item?.handphone} / {item?.address}
+                </td>
+                <td class="border border-gray-300 text-center">{item?.name}</td>
+                <td class="border border-gray-300 text-cente">{item?.total}</td>
+                <td class="border border-gray-300 text-center">
+                  {item?.quantity}
+                </td>
+                <td
+                  class={`border border-gray-300 text-center ${
+                    item?.status === "waiting"
+                      ? "text-yellow-300"
+                      : item?.status.toLowerCase() === "success"
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }`}
+                >
+                  {item?.status}
+                </td>
+                <td class="border border-gray-300 text-center">
+                  {item?.status === "waiting" ? (
+                    <div className="p-3">
+                      <PrimaryButton
+                        title="Cancel"
+                        onClick={() => onCancelCheckout(item?.id)}
+                      />
+                      <div className="h-4" />
+                      <PrimaryButton
+                        title="Accept"
+                        onClick={() => onConfirmCheckout(item?.id)}
+                      />
+                    </div>
+                  ) : (
+                    <p>Done</p>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </MainLayout>
   );
 };
